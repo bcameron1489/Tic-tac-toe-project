27ccc5dc3a2c13d689b545e103931c396653f3ed
bcameron1489 <bcameron1489@gmail.com>
2017-11-03 15:25:37 -0400

    Debugging jquery

---
+++ assets/scripts/main.js
@@ -0,0 +1,51 @@
+require('./assets/styles/index.scss')
+
+const sqOne = document.getElementById('position-one')
+const sqTwo = document.getElementById('position-two')
+const sqThree = document.getElementById('position-three')
+const sqFour = document.getElementById('position-four')
+const sqFive = document.getElementById('position-five')
+const sqSix = document.getElementById('position-six')
+const sqSeven = document.getElementById('position-seven')
+const sqEight = document.getElementById('position-eight')
+const sqNine = document.getElementById('position-nine')
+
+sqOne.addEventListener('click', function () {
+  console.log('testing postition 1')
+})
+
+sqTwo.addEventListener('click', function () {
+  console.log('testing postition 2')
+})
+
+sqThree.addEventListener('click', function () {
+  console.log('testing postition 3')
+})
+
+sqFour.addEventListener('click', function () {
+  console.log('testing postition 4')
+})
+
+sqFive.addEventListener('click', function () {
+  console.log('testing postition 5')
+})
+
+sqSix.addEventListener('click', function () {
+  console.log('testing postition 6')
+})
+
+sqSeven.addEventListener('click', function () {
+  console.log('testing postition 7')
+})
+
+sqEight.addEventListener('click', function () {
+  console.log('testing postition 8')
+})
+
+sqNine.addEventListener('click', function () {
+  console.log('testing postition 9')
+})
+
+$('#position-one').on('click', function () {
+  this.html('X')
+})
