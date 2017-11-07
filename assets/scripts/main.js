b59ccee15390230a3cc1c97e36dbfe387f810ac4
bcameron1489 <bcameron1489@gmail.com>
2017-11-07 12:48:19 -0500

    Added show winner and function triggers
    Add show winners function to show winners on left of page.
    Add logic to grab html and run it through the winners function.
    Switch the order of the turn switch and the checkWinner function
    so the trigger shows on click not on the following click.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,14 +6,56 @@ const tileSelect = function () {
   // moves: (0)
   }
   // alternate turns
-  $('.game-piece').click(function () {
-    if (gameInfo.turn % 2 === 0) {
-      $(this).html('X')
-    } else {
-      $(this).html('O')
-    }
-    gameInfo.turn += 1
-  })
+  const takeTurns = function () {
+    $('.game-piece').on('click', function (event) {
+      if (gameInfo.turn % 2 === 0) {
+        $(this).html('X')
+      } else if
+      (gameInfo.turn % 2 === 1) {
+        $(this).html('O')
+      } else {
+        console.log('gameover')
+      }
+
+      gameInfo.turn += 1
+    })
+  }
+  const checkWinner = function () {
+    const showWinner = $('.winner')
+    $('.game-piece').click(function (event) {
+      const sq0 = $('#tile-zero').html()
+      const sq1 = $('#tile-one').html()
+      const sq2 = $('#tile-two').html()
+      const sq3 = $('#tile-three').html()
+      const sq4 = $('#tile-four').html()
+      const sq5 = $('#tile-five').html()
+      const sq6 = $('#tile-six').html()
+      const sq7 = $('#tile-seven').html()
+      const sq8 = $('#tile-eight').html()
+
+      if (sq0 !== '' & sq0 === sq1 & sq1 === sq2) {
+        return $(showWinner).html(sq0 + ' Wins')
+      } else if (sq3 !== '' & sq3 === sq4 & sq4 === sq5) {
+        return $(showWinner).html(sq1 + ' Wins')
+      } else if (sq6 !== '' & sq6 === sq7 & sq7 === sq8) {
+        return $(showWinner).html(sq2 + ' Wins')
+      } else if (sq0 !== '' & sq0 === sq3 & sq3 === sq6) {
+        return $(showWinner).html(sq3 + ' Wins')
+      } else if (sq1 !== '' & sq1 === sq4 & sq4 === sq7) {
+        return $(showWinner).html(sq4 + ' Wins')
+      } else if (sq2 !== '' & sq2 === sq5 & sq5 === sq8) {
+        return $(showWinner).html(sq5 + ' Wins')
+      } else if (sq0 !== '' & sq0 === sq4 & sq4 === sq8) {
+        return $(showWinner).html(sq6 + ' Wins')
+      } else if (sq6 !== '' & sq6 === sq4 & sq4 === sq2) {
+        return $(showWinner).html(sq7 + ' Wins')
+      } else { // no winner so return "false"
+        console.log('false')
+      }
+    })
+  }
+  takeTurns()
+  checkWinner()
   // display turn
   const gameBoard = $('.turn')
   $('#game-grid').click(function () {
