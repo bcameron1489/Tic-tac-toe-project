a5cc7bcca91fffb825faca3cda3f32968b201da0
bcameron1489 <bcameron1489@gmail.com>
2017-11-07 10:56:16 -0500

    Add show player turn
    shows player turn in h4 above game board

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,65 +1,11 @@
-/*    not needed due to jquery
-const grid = {
-  sqOne: document.getElementById('position-one'),
-  sqTwo: document.getElementById('position-two'),
-  sqThree: document.getElementById('position-three'),
-  sqFour: document.getElementById('position-four'),
-  sqFive: document.getElementById('position-five'),
-  sqSix: document.getElementById('position-six'),
-  sqSeven: document.getElementById('position-seven'),
-  sqEight: document.getElementById('position-eight'),
-  sqNine: document.getElementById('position-nine')
-}
-*/
+// turn game logic
 
-// event listeners for visual aid - commented out, obsolete due to jquery
-/*
-  grid.sqOne.addEventListener('click', function () {
-    console.log('testing postition 1')
-  })
-// trim the fat here... can i use 1 event handler for click.
-  grid.sqTwo.addEventListener('click', function () {
-    console.log('testing postition 2')
-  })
-
-  grid.sqThree.addEventListener('click', function () {
-    console.log('testing postition 3')
-  })
-
-  grid.sqFour.addEventListener('click', function () {
-    console.log('testing postition 4')
-  })
-
-  grid.sqFive.addEventListener('click', function () {
-    console.log('testing postition 5')
-  })
-
-  grid.sqSix.addEventListener('click', function () {
-    console.log('testing postition 6')
-  })
-
-  grid.sqSeven.addEventListener('click', function () {
-    console.log('testing postition 7')
-  })
-
-  grid.sqEight.addEventListener('click', function () {
-    console.log('testing postition 8')
-  })
-
-  grid.sqNine.addEventListener('click', function () {
-    console.log('testing postition 9')
-  })
-*/
-// jquery
-
-const addHandler = function () {
+const tileSelect = function () {
   const gameInfo = {
-  //  playerOne: ('X')         use later
-  //  playerTwo: ('O')            ||
     turn: (0)
-  //  moves: (0)
+  // moves: (0)
   }
-  gameInfo.turn = 0
+  // alternate turns
   $('.game-piece').click(function () {
     if (gameInfo.turn % 2 === 0) {
       $(this).html('X')
@@ -68,8 +14,19 @@ const addHandler = function () {
     }
     gameInfo.turn += 1
   })
+  // display turn
+  const gameBoard = $('.turn')
+  $('#game-grid').click(function () {
+    if (gameInfo.turn === 9) {
+      $(gameBoard).html('Game Over')
+    } else if
+    (gameInfo.turn % 2 === 1) {
+      $(gameBoard).html('Player Two!')
+    } else {
+      $(gameBoard).html('Player One!')
+    }
+  })
 }
-
 module.exports = {
-  addHandler
+  tileSelect
 }
