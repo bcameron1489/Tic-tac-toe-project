0d215f1b395c32dc4672f46212ded6d0bf7bab38
bcameron1489 <bcameron1489@gmail.com>
2017-11-07 22:54:11 -0500

    Tweak game logic for winner and clear board
    clear board after win and cycle turn back to x after win

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,12 +14,10 @@ const tileSelect = function () {
       if ($(this).html() === 'X' || $(this).html() === 'O') {
         $(gameBoard).html('Pick again!')
         return null
-      } else if
-      (gameInfo.turn % 2 === 0) {
+      } else if (gameInfo.turn % 2 === 0) {
         $(this).html('X')
         $(gameBoard).html('Player 2\'s Turn!')
-      } else if
-      (gameInfo.turn % 2 === 1) {
+      } else if (gameInfo.turn % 2 === 1) {
         $(this).html('O')
         $(gameBoard).html('Player 1\'s Turn!')
       } else {
@@ -47,26 +45,50 @@ const tileSelect = function () {
 
       if (sq0 !== '' & sq0 === sq1 & sq1 === sq2) {
         $('.game-piece').html('')
+        $(gameBoard).html('Player 1\'s Turn!')
+        gameInfo.turn = 0
         return $(showWinner).html(sq0 + ' Wins')
       } else if (sq3 !== '' & sq3 === sq4 & sq4 === sq5) {
+        $('.game-piece').html('')
+        $(gameBoard).html('Player 1\'s Turn!')
+        gameInfo.turn = 0
         return $(showWinner).html(sq3 + ' Wins')
       } else if (sq6 !== '' & sq6 === sq7 & sq7 === sq8) {
+        $('.game-piece').html('')
+        $(gameBoard).html('Player 1\'s Turn!')
+        gameInfo.turn = 0
         return $(showWinner).html(sq6 + ' Wins')
       } else if (sq0 !== '' & sq0 === sq3 & sq3 === sq6) {
+        $('.game-piece').html('')
+        $(gameBoard).html('Player 1\'s Turn!')
+        gameInfo.turn = 0
         return $(showWinner).html(sq0 + ' Wins')
       } else if (sq1 !== '' & sq1 === sq4 & sq4 === sq7) {
+        $('.game-piece').html('')
+        $(gameBoard).html('Player 1\'s Turn!')
+        gameInfo.turn = 0
         return $(showWinner).html(sq1 + ' Wins')
       } else if (sq2 !== '' & sq2 === sq5 & sq5 === sq8) {
+        $('.game-piece').html('')
+        $(gameBoard).html('Player 1\'s Turn!')
+        gameInfo.turn = 0
         return $(showWinner).html(sq2 + ' Wins')
       } else if (sq0 !== '' & sq0 === sq4 & sq4 === sq8) {
+        $('.game-piece').html('')
+        $(gameBoard).html('Player 1\'s Turn!')
+        gameInfo.turn = 0
         return $(showWinner).html(sq0 + ' Wins')
       } else if (sq6 !== '' & sq6 === sq4 & sq4 === sq2) {
+        $('.game-piece').html('')
+        $(gameBoard).html('Player 1\'s Turn!')
+        gameInfo.turn = 0
         return $(showWinner).html(sq6 + ' Wins')
       } else { // no winner so return "false"
         return false
       }
     })
   }
+
   // function triggers making sure turn goes before winner is checked or
   // else it will take an additional click to register the html element for win
   takeTurns()
