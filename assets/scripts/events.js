d68aaf14b481d913af69289e80b82a1afaa09e45
bcameron1489 <bcameron1489@gmail.com>
2017-11-08 19:07:40 -0500

    Typo in events
    Fix typo on 'OnSignUpSuccess' --- onSignUpSuccess

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,16 +2,16 @@ const getFormFields = require('../../lib/get-form-fields')
 const api = require('./api')
 const ui = require('./ui')
 
-const addHandlers = function () {
-  $('#sign-up').on('submit', onSignUp)
-}
-
 const onSignUp = function (event) {
   event.preventDefault()
   const data = getFormFields(this)
   api.signUp(data)
-    .then(ui.OnSignUpSuccess)
+    .then(ui.onSignUpSuccess)
     .catch(ui.onSignUpFailure)
+}
+
+const addHandlers = function () {
+  $('#sign-up').on('submit', onSignUp)
 }
 
 module.exports = {
