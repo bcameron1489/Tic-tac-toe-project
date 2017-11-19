62c1aa9434b9f42571e29eab1d1b19b4d0f581b4
bcameron1489 <bcameron1489@gmail.com>
2017-11-19 03:20:30 -0500

    Remove .hide on signout and center table
    remove .hide on signout success in ui and center table for all sizes

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -4,7 +4,6 @@ const main = require('./main')
 // Success & Failure message display on Sign up
 const onSignUpSuccess = function (data) {
   $('.alerts').html('Thank you for signing up!')
-  $('#sign-up').hide()
 }
 
 const onSignUpFailure = function () {
